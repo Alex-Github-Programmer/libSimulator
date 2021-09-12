2abf816afff809f9adc1677691983a62a4646c4c
Alex-Github-Programmer <55904367+Alex-Github-Programmer@users.noreply.github.com>
2021-09-12 15:21:39 +0800

    Add files via upload

--- SUGE.c
+++ SUGE.c
@@ -1,37 +1,25 @@
 /**
- * Name: SUGE.c v2.3.0
+ * Name: SUGE.c v2.3.1
  * Date: 2021-09-12
  * Intro: A lib to simulate astronomical phenomenons.
- * Update: Use gdiTemplate.h. Change configure file from SUGE.cfg2 to SUGE and 
- *		allow to appoint configure file. Upgrade configure file from v2 to v3.
+ * Update: Allows two colors to display clearly, now supports v4.
  * Open configure file SUGE by default, which can be appointed by cmd argument:
- * - v2:
- *		ver: 2
- *		num: Numbers of celestial bodies
- *		X position (a list of length num)
- *		Y position (a list of length num)
- *		X velocity (a list of length num)
- *		Y velocity (a list of length num)
- *		Quality (a list of length num)
- *		Foreground color (a list of length num in hex BBGGRR)
+ *		ver: 2/3/4
+ *		number: Numbers of celestial bodies
+ *		X position (a list of length number)
+ *		Y position (a list of length number)
+ *		X velocity (a list of length number)
+ *		Y velocity (a list of length number)
+ *		Quality (a list of length number)
+ *		[Added: v3] Width (a list of length number)
+ *		[Deprecated: v4] Foreground color (a list of length number in hex BBGGRR)
+ *		[Added: v4] Foreground color #1
+ *		[Added: v4] Foreground color #2
  *		Background color
  *		Simulate accuracy
- *		Display frequent (ticks in each display)
- *		Width of the line
- *		X/Y offset
- * - v3:
- *		ver: 3
- *		num: Numbers of celestial bodies
- *		X position (a list of length num)
- *		Y position (a list of length num)
- *		X velocity (a list of length num)
- *		Y velocity (a list of length num)
- *		Quality (a list of length num)
- *		Width (a list of length num)
- *		Foreground color (a list of length num in hex BBGGRR)
- *		Background color
- *		Simulate accuracy
- *		Display frequent (ticks in each display)
+ *		Display frequent (ticks in each frame)
+ *		[Deprecated: v3] Width
+ *		[Added: v4] Switch (frames in each switching colors)
  *		X/Y offset
  */
 
@@ -45,90 +33,95 @@ COLORREF BACKGROUND_COLOR;
 
 #include "gdiTemplate.c"
 
-#ifdef USE_SSE
-#include <xmmintrin.h>
-__m128 m, n;
+#ifdef _SIMULATOR_USE_OMP
+#include <omp.h>
 #endif
 
-int ver = -1, num, display_freq, offset[2], count, i, j;
-double simulate_acc, distance, p, q, r, s;
+int version, number, displayFreq, switchTicks = 1, offset[2], ticks, i, parity;
+double simulateAcc;
 LPSTR fileName = "SUGE";
 DWORD WINAPI threadProc(LPVOID lpParamter) {
 	FILE *file = fopen(fileName, "r");
 	HWND hwnd = (HWND)lpParamter;
-	fscanf(file, "%d%d", &ver, &num);
-	HDC hdc[num];
-	HPEN hpen[num];
-	double position[num][2], velocity[num][2], quality[num];
-	int width[num];
-	COLORREF color[num];
-	if(ver == 2) {
-		for(i=0; i<num; ++i) fscanf(file, "%lf", &position[i][0]);
-		for(i=0; i<num; ++i) fscanf(file, "%lf", &position[i][1]);
-		for(i=0; i<num; ++i) fscanf(file, "%lf", &velocity[i][0]);
-		for(i=0; i<num; ++i) fscanf(file, "%lf", &velocity[i][1]);
-		for(i=0; i<num; ++i) fscanf(file, "%lf", &quality[i]);
-		for(i=0; i<num; ++i) fscanf(file, "%lx", &color[i]);
-		fscanf(file, "%lx %lf %d %d %d %d", &BACKGROUND_COLOR, &simulate_acc, &display_freq, &width[0], &offset[0], &offset[1]);
-		for(i=0; i<num; ++i) width[i] = width[0];
-	} else if(ver == 3) {
-		for(i=0; i<num; ++i) fscanf(file, "%lf", &position[i][0]);
-		for(i=0; i<num; ++i) fscanf(file, "%lf", &position[i][1]);
-		for(i=0; i<num; ++i) fscanf(file, "%lf", &velocity[i][0]);
-		for(i=0; i<num; ++i) fscanf(file, "%lf", &velocity[i][1]);
-		for(i=0; i<num; ++i) fscanf(file, "%lf", &quality[i]);
-		for(i=0; i<num; ++i) fscanf(file, "%d", &width[i]);
-		for(i=0; i<num; ++i) fscanf(file, "%lx", &color[i]);
-		fscanf(file, "%lx %lf %d %d %d", &BACKGROUND_COLOR, &simulate_acc, &display_freq, &offset[0], &offset[1]);
+	fscanf(file, "%d%d", &version, &number);
+	HDC hdc[number][2];
+	HPEN hpen[number][2];
+	double position[number][2], velocity[number][2], quality[number];
+	int width[number];
+	COLORREF color[number][2];
+	if(version==2 || version==3 || version==4) {
+		for(i=0; i<number; ++i) fscanf(file, "%lf", &position[i][0]);
+		for(i=0; i<number; ++i) fscanf(file, "%lf", &position[i][1]);
+		for(i=0; i<number; ++i) fscanf(file, "%lf", &velocity[i][0]);
+		for(i=0; i<number; ++i) fscanf(file, "%lf", &velocity[i][1]);
+		for(i=0; i<number; ++i) fscanf(file, "%lf", &quality[i]);
+		if(version >= 3) for(i=0; i<number; ++i) fscanf(file, "%d", &width[i]);
+		for(i=0; i<number; ++i) fscanf(file, "%lx", &color[i][0]);
+		if(version < 4) for(i=0; i<number; ++i) color[i][1] = color[i][0];
+		else for(i=0; i<number; ++i) fscanf(file, "%lx", &color[i][1]);
+		fscanf(file, "%lx %lf %d", &BACKGROUND_COLOR, &simulateAcc, &displayFreq);
+		if(version == 2) {
+			fscanf(file, "%d", &width[0]);
+			for(i=0; i<number; ++i) width[i] = width[0];
+		} else if(version == 4) {
+			fscanf(file, "%d", &switchTicks);
+		}
+		fscanf(file, "%d %d", &offset[0], &offset[1]);
+		
 	} else {
-		printf("Version not support: %d", ver);
+		printf("Version not support: %d", version);
 		return 1;
 	}
 	fclose(file);
-	for(i=0; i<num; ++i) {
-		quality[i] *= simulate_acc * simulate_acc;
-		velocity[i][0] *= simulate_acc;
-		velocity[i][1] *= simulate_acc;
-	}
-	for(i=0; i<num; ++i) {
-		hdc[i] = GetDC(hwnd);
-		hpen[i] = CreatePen(PS_SOLID, width[i], color[i]);
-		SelectObject(hdc[i], hpen[i]);
+	for(i=0; i<number; ++i) {
 		position[i][0] += offset[0];
 		position[i][1] += offset[1];
-		MoveToEx(hdc[i], position[i][0], position[i][1], 0);
+		velocity[i][0] *= simulateAcc;
+		velocity[i][1] *= simulateAcc;
+		quality[i] *= simulateAcc * simulateAcc;
+	}
+	switchTicks *= displayFreq;
+	for(i=0; i<number; ++i) {
+		int j;
+		for(j=0; j<2; ++j) {
+			hdc[i][j] = GetDC(hwnd);
+			hpen[i][j] = CreatePen(PS_SOLID, width[i], color[i][j]);
+			SelectObject(hdc[i][j], hpen[i][j]);
+			MoveToEx(hdc[i][j], position[i][0], position[i][1], 0);
+		}
 	}
 	while(1) {
-		for(i=0; i<num; ++i) {
-			for(j=0; j<i; ++j) {
-				p = position[j][0] - position[i][0];
-				q = position[j][1] - position[i][1];
-				if(p==0 && q==0) continue;
-#ifdef USE_SSE
-				m = _mm_set1_ps(p * p + q * q);
-				n = _mm_rsqrt_ps(m);
-				distance = n[0] * n[0] * n[0];
-#else
-				distance = pow(p * p + q * q, -1.5);
+#ifdef _SIMULATOR_USE_OMP
+#pragma omp parallel for
 #endif
-				r = quality[j] * distance;
-				s = quality[i] * distance;
-				velocity[i][0] += p * r;
-				velocity[i][1] += q * r;
-				velocity[j][0] -= p * s;
-				velocity[j][1] -= q * s;
+		for(i=0; i<number; ++i) {
+			int j;
+			for(j=0; j<i; ++j) {
+				double distance_neg3, differenceX, differenceY, quality_j_distance_neg3, quality_i_distance_neg3;
+				differenceX = position[j][0] - position[i][0];
+				differenceY = position[j][1] - position[i][1];
+				if(differenceX==0 && differenceY==0) continue;
+				distance_neg3 = pow(differenceX * differenceX + differenceY * differenceY, -1.5);
+				quality_j_distance_neg3 = quality[j] * distance_neg3;
+				quality_i_distance_neg3 = quality[i] * distance_neg3;
+				velocity[i][0] += differenceX * quality_j_distance_neg3;
+				velocity[i][1] += differenceY * quality_j_distance_neg3;
+				velocity[j][0] -= differenceX * quality_i_distance_neg3;
+				velocity[j][1] -= differenceY * quality_i_distance_neg3;
 			}
 		}
-		if(count % display_freq == 0) {
-			for(i=0; i<num; ++i) {
-				LineTo(hdc[i], position[i][0], position[i][1]);
+		if(ticks % displayFreq == 0) {
+			parity = (ticks / switchTicks) & 1;
+			for(i=0; i<number; ++i) {
+				LineTo(hdc[i][parity], position[i][0], position[i][1]);
+				MoveToEx(hdc[i][parity^1], position[i][0], position[i][1], 0);
 			}
 		}
-		for(i=0; i<num; ++i) {
+		for(i=0; i<number; ++i) {
 			position[i][0] += velocity[i][0];
 			position[i][1] += velocity[i][1];
 		}
-		++count;
+		++ticks;
 	}
 }
 
